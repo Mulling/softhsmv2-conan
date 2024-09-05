4dbe81e6c4b475e17b0e39b43ebcf26d16407e83
Mulling <lmulling@proton.me>
2024-09-04 23:32:21 -0300

    Update package license and description

--- conanfile.py
+++ conanfile.py
@@ -2,22 +2,14 @@ from conans import ConanFile, AutoToolsBuildEnvironment, tools
 import os
 
 
-class Softhsmv2Conan(ConanFile):
+class SoftHSMv2(ConanFile):
     name = "softhsmv2"
     version = "2.6.1"
-    license = "<Put the package license here>"
+    license = "BSD-2-Clause"
     author = "OpenDNSSEC"
     url = "https://github.com/opendnssec/SoftHSMv2"
     releases = "https://github.com/opendnssec/SoftHSMv2/archive/refs/tags/%s.tar.gz"
-    description = """
-A potential problem with the use of the PKCS#11 interface is that it might
-limit the wide spread use of OpenDNSSEC, since a potential user might not be
-willing to invest in a new hardware device. To counter this effect, OpenDNSSEC
-is providing a software implementation of a generic cryptographic device with a
-PKCS#11 interface, the SoftHSM. SoftHSM is designed to meet the requirements of
-OpenDNSSEC, but can also work together with other cryptographic products
-because of the PKCS#11 interface.
-    """
+    description = "Conan package for the SoftHSM version 2, part of the OpenDNSSEC project."
     settings = "compiler"
     generators = "make"
 
